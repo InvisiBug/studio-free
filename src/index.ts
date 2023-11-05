64d0325349186e6286db79701926f619984df3e3
InvisiBug <matthewjkavanagh@gmail.com>
2023-11-05 23:47:13 +0000

    Used copolit to create an array of free times, not quite working

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,28 @@ axios.get("https://businessgateway.puregym.com/api/bookings/v1/timetable/75/sche
   let studioInUseArray: any[] = [];
   let studioInUseToday: any[] = [];
 
+  let freeTimes: any[] = [];
+
+  //* need to limit the response to the current day period
+
   data.activities.forEach((element: any, index: number) => {
     if (element.studio === "Studio") {
-      const now = new Date().getTime();
-      const timeInUse = Date.parse(element.startDateTime.dateTime);
+      // const now2 = new Date();
+      const now2 = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+      const date2 = new Date(Date.parse(element.startDateTime.dateTime));
 
-      let Difference_In_Days = (timeInUse - now) / (1000 * 3600 * 24);
+      if (now2.setHours(0, 0, 0, 0) == date2.setHours(0, 0, 0, 0)) {
+        //* This event is today
+        // Date equals today's date
+        console.log("today");
+        console.log(now2.setHours(0, 0, 0, 0), date2.setHours(0, 0, 0, 0));
 
-      if (Difference_In_Days <= 1) {
+        const now = new Date().getTime();
+        const timeInUse = Date.parse(element.startDateTime.dateTime);
+
+        let Difference_In_Days = (timeInUse - now) / (1000 * 3600 * 24);
+
+        // if (Difference_In_Days <= 1) {
         const startHours = new Date(element.startDateTime.dateTime).getHours();
         const startMinutes = String(new Date(element.startDateTime.dateTime).getMinutes()).padStart(2, "0");
 
@@ -25,18 +39,46 @@ axios.get("https://businessgateway.puregym.com/api/bookings/v1/timetable/75/sche
           start: `${startHours}:${startMinutes}`,
           end: `${endHours}:${endMinutes}`,
           duration: element.duration,
-          start2: timeBetweenDates(new Date(element.startDateTime.dateTime), new Date(Date.parse(element.startDateTime.dateTime) + element.duration * 60000)),
         });
+        // }
       }
     }
   });
 
   console.log(studioInUseToday);
-  console.log("Studio is in use during the following hours (within 24 hours):");
+
+  console.log("Studio is in use during the following hours today:");
   studioInUseToday.forEach((element) => {
     console.log(`${element.start} - ${element.end} `);
   });
+
+  let freeTimesToday = getFreeTimes(studioInUseToday);
+  console.log("Studio is free during the following hours today:");
+  freeTimesToday.forEach((element) => {
+    console.log(`${element.start} - ${element.end}`);
+  });
 });
+
+function getFreeTimes(inUseTimes: { start: string; end: string }[]): { start: string; end: string }[] {
+  // Sort the in-use times
+  inUseTimes.sort((a, b) => a.start.localeCompare(b.start));
+
+  let freeTimes: { start: string; end: string }[] = [];
+  let freeTimeEnd = "00:00";
+
+  for (let inUseTime of inUseTimes) {
+    if (freeTimeEnd !== inUseTime.start) {
+      freeTimes.push({ start: freeTimeEnd, end: inUseTime.start });
+    }
+    freeTimeEnd = inUseTime.end;
+  }
+
+  if (freeTimeEnd !== "23:59") {
+    freeTimes.push({ start: freeTimeEnd, end: "23:59" });
+  }
+
+  return freeTimes;
+}
 
 function timeBetweenDates(date1: Date, date2: Date): string {
   const diffInMs = Math.abs(date2.getTime() - date1.getTime());
@@ -47,8 +89,3 @@ function timeBetweenDates(date1: Date, date2: Date): string {
   const seconds = diffInSecs % 60;
   return `${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
 }
-
-// Usage
-const date1 = new Date("2022-01-01");
-const date2 = new Date("2022-01-02");
-console.log(timeBetweenDates(date1, date2));
