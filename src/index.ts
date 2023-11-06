b82b2a2608a2f41511c6323f55bed9269c6abcd5
InvisiBug <matthewjkavanagh@gmail.com>
2023-11-06 00:10:50 +0000

    Need to add multiple days and a table

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,15 @@ axios.get("https://businessgateway.puregym.com/api/bookings/v1/timetable/75/sche
     if (element.studio === "Studio") {
       // const now = new Date();
       const now = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+      // const now = new Date(new Date());
       const date = new Date(Date.parse(element.startDateTime.dateTime));
 
-      // Get olnly the events for the day
+      // Get only the events for the day
       if (now.setHours(0, 0, 0, 0) == date.setHours(0, 0, 0, 0)) {
         const startHours = String(new Date(element.startDateTime.dateTime).getHours()).padStart(2, "0");
         const startMinutes = String(new Date(element.startDateTime.dateTime).getMinutes()).padStart(2, "0");
 
-        const endHours = new Date(Date.parse(element.startDateTime.dateTime) + element.duration * 60000).getHours();
+        const endHours = String(new Date(Date.parse(element.startDateTime.dateTime) + element.duration * 60000).getHours()).padStart(2, "0");
         const endMinutes = String(new Date(Date.parse(element.startDateTime.dateTime) + element.duration * 60000).getMinutes()).padStart(2, "0");
 
         studioInUseToday.push({
